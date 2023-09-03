5cc2b54c8cf9d3e711c573e00bed455e32756629
Ryan Adolf <ryanadolf123@gmail.com>
2023-09-03 10:51:03 -0400

    Simplify action handling

--- src/lib/handle-action.ts
+++ src/lib/handle-action.ts
@@ -5,10 +5,7 @@
  */
 
 import { HassServiceTarget } from "home-assistant-js-websocket";
-import { forwardHaptic } from "custom-card-helpers";
 import { fireEvent } from "custom-card-helpers";
-import { navigate } from "custom-card-helpers";
-import { toggleEntity } from "custom-card-helpers";
 
 type HomeAssistant = any; // Hack since the custom-card-helpers HomeAssistant type is out of date
 
@@ -70,90 +67,9 @@ export type ActionConfig =
   | NoActionConfig
   | CustomActionConfig;
 
-export const handleActionConfig = (
-  node: HTMLElement,
-  hass: HomeAssistant,
-  config: {
-    entity?: string;
-    camera_image?: string;
-    hold_action?: ActionConfig;
-    tap_action?: ActionConfig;
-    double_tap_action?: ActionConfig;
-  },
-  actionConfig: ActionConfig | undefined
-): void => {
-  if (!actionConfig) {
-    actionConfig = {
-      action: "more-info",
-    };
-  }
-
-  if (
-    actionConfig.confirmation &&
-    (!actionConfig.confirmation.exemptions ||
-      !actionConfig.confirmation.exemptions.some(
-        (e) => e.user === hass!.user!.id
-      ))
-  ) {
-    forwardHaptic("warning");
-
-    if (
-      !confirm(
-        actionConfig.confirmation.text ||
-          `Are you sure you want to ${actionConfig.action}?`
-      )
-    ) {
-      return;
-    }
-  }
-
-  switch (actionConfig.action) {
-    case "more-info":
-      if (config.entity || config.camera_image) {
-        fireEvent(node, "hass-more-info", {
-          entityId: config.entity ? config.entity : config.camera_image!,
-        });
-      }
-      break;
-    case "navigate":
-      if (actionConfig.navigation_path) {
-        navigate(node, actionConfig.navigation_path);
-      }
-      break;
-    case "url":
-      if (actionConfig.url_path) {
-        window.open(actionConfig.url_path);
-      }
-      break;
-    case "toggle":
-      if (config.entity) {
-        toggleEntity(hass, config.entity!);
-        forwardHaptic("success");
-      }
-      break;
-    case "call-service": {
-      if (!actionConfig.service) {
-        forwardHaptic("failure");
-        return;
-      }
-      const [domain, service] = actionConfig.service.split(".", 2);
-      hass.callService(domain,
-                       service,
-                       actionConfig.data ?? actionConfig.service_data,
-                       actionConfig.target
-                      );
-      forwardHaptic("light");
-      break;
-    }
-    case "fire-dom-event": {
-      fireEvent(node, "ll-custom", actionConfig);
-    }
-  }
-};
-
 export const handleAction = (
   node: HTMLElement,
-  hass: HomeAssistant,
+  _hass: HomeAssistant,
   config: {
     entity?: string;
     camera_image?: string;
@@ -163,15 +79,6 @@ export const handleAction = (
   },
   action: string
 ): void => {
-  let actionConfig: ActionConfig | undefined;
-
-  if (action === "double_tap" && config.double_tap_action) {
-    actionConfig = config.double_tap_action;
-  } else if (action === "hold" && config.hold_action) {
-    actionConfig = config.hold_action;
-  } else if (action === "tap" && config.tap_action) {
-    actionConfig = config.tap_action;
-  }
-
-  handleActionConfig(node, hass, config, actionConfig);
+  // @ts-ignore: hass-action isn't in custom-card-helpers
+  fireEvent(node, "hass-action", { config, action });
 };
