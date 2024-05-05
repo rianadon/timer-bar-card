c0dff76460eeba4caef4f50ef09d3996c57f5c7b
SLaks <Dev@SLaks.net>
2024-05-05 13:06:22 -0400

    Fix negative rounding consistency

--- src/format-time.ts
+++ src/format-time.ts
@@ -8,7 +8,7 @@
     EDIT: This has been turned into a general-purpose time formatting library.
 */
 
-type Resolution = "seconds"|"minutes"|"automatic";
+type Resolution = "seconds" | "minutes" | "automatic";
 
 
 const leftPad = (num: number) => {
@@ -22,8 +22,8 @@ const roundUp = (num: number) => num > 0 ? Math.ceil(num) : Math.floor(num);
 
 /** Returns "minutes" if seconds>=1hr, otherwise minutes. */
 function automaticRes(seconds: number) {
-    if (seconds >= 3600) return "minutes"
-    return "seconds"
+  if (seconds >= 3600) return "minutes"
+  return "seconds"
 }
 
 
@@ -51,32 +51,44 @@ const hmTime = (d: number) => {
   return joinWithColons(0, h, m);
 }
 
+/** Like Math.truncate(), but truncates `-.5` to `'-0'` */
+function truncateWithSign(d: number): string {
+  const result = Math.trunc(d);
+  if (d < 0 && result === 0) return `-${-result}`;
+  return result.toString();
+}
+
 export default function formatTime(d: number, format: string) {
   if (format == 'hms') return hmsTime(d)
   if (format == 'hm') return hmTime(d)
-  if (format == 'd') return ''+roundUp(d / 24 / 3600)
-  if (format == 'h') return ''+roundUp(d / 3600)
-  if (format == 'm') return ''+roundUp(d / 60)
-  if (format == 's') return ''+roundUp(d)
 
+  // When rendering a single component, always round up to count consistent whole units.
+  if (format == 'd') return '' + Math.ceil(d / 24 / 3600)
+  if (format == 'h') return '' + Math.ceil(d / 3600)
+  if (format == 'm') return '' + Math.ceil(d / 60)
+  if (format == 's') return '' + Math.ceil(d)
+
+
+  // When rendering multiple components, round towards zero because the fraction should
+  // be represented by the next unit.
   return format.replace(/%(\w+)/g, (match, S) => {
     const sl = S.toLowerCase()
     if (sl.startsWith('hms')) return hmsTime(d) + S.substring(3)
     if (sl.startsWith('hm')) return hmTime(d) + S.substring(2)
     // 1 lowercase letter: round up
-    if (S.startsWith('d')) return roundUp(d / 24 / 3600) + S.substring(1)
-    if (S.startsWith('h')) return roundUp(d / 3600) + S.substring(1)
-    if (S.startsWith('m')) return roundUp(d / 60) + S.substring(1)
-    if (S.startsWith('s')) return roundUp(d) + S.substring(1)
+    if (S.startsWith('d')) return Math.ceil(d / 24 / 3600) + S.substring(1)
+    if (S.startsWith('h')) return Math.ceil(d / 3600) + S.substring(1)
+    if (S.startsWith('m')) return Math.ceil(d / 60) + S.substring(1)
+    if (S.startsWith('s')) return Math.ceil(d) + S.substring(1)
     // 2 capital letter: pad + round down
     if (S.startsWith('HH')) return leftPad(Math.trunc((d % (3600 * 24)) / 3600)) + S.substring(2)
     if (S.startsWith('MM')) return leftPad(Math.trunc((d % 3600) / 60)) + S.substring(2)
     if (S.startsWith('SS')) return leftPad(Math.trunc(d % 60)) + S.substring(2)
-    // 1 capital letter: round down
-    if (S.startsWith('D')) return Math.trunc(d / 24 / 3600) + S.substring(1)
-    if (S.startsWith('H')) return Math.trunc((d % (3600 * 24)) / 3600) + S.substring(1)
-    if (S.startsWith('M')) return Math.trunc((d % 3600) / 60) + S.substring(1)
-    if (S.startsWith('S')) return Math.trunc(d % 60) + S.substring(1)
+    // 1 capital letter: round down, always include sign (for next component)
+    if (S.startsWith('D')) return truncateWithSign(d / 24 / 3600) + S.substring(1)
+    if (S.startsWith('H')) return truncateWithSign((d % (3600 * 24)) / 3600) + S.substring(1)
+    if (S.startsWith('M')) return truncateWithSign((d % 3600) / 60) + S.substring(1)
+    if (S.startsWith('S')) return truncateWithSign(d % 60) + S.substring(1)
     return match
   })
 }
